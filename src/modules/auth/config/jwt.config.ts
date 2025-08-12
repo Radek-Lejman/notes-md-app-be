afbb021cda7b65f4b8882ecfe6b10334bc7bef7b
Radek <radoslawlejman@gmail.com>
2025-08-12 23:07:46 +0200

    fix(jwt): use runtime-compatible {secret, expiresIn} in refresh-jwt and jwt configs

--- src/modules/auth/config/jwt.config.ts
+++ src/modules/auth/config/jwt.config.ts
@@ -2,10 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { JwtSignOptions } from '@nestjs/jwt';
 import { jwtAccessTokenConstant } from '../constants';
 
-export default registerAs(
-  'jwt',
-  (): JwtSignOptions => ({
-    accessTokenSecret: jwtAccessTokenConstant.secret,
-    accessTokenExpiresIn: jwtAccessTokenConstant.expire_in,
-  }),
-);
+export default registerAs('jwt', () => ({
+  secret: jwtAccessTokenConstant.secret,
+  expiresIn: jwtAccessTokenConstant.expire_in,
+}));
--- src/modules/auth/config/refreshJwt.config.ts
+++ src/modules/auth/config/refreshJwt.config.ts
@@ -1,11 +1,7 @@
 import { registerAs } from '@nestjs/config';
-import { JwtSignOptions } from '@nestjs/jwt';
 import { jwtRefreshTokenConstants } from '../constants';
 
-export default registerAs(
-  'refresh-jwt',
-  (): JwtSignOptions => ({
-    accessTokenSecret: jwtRefreshTokenConstants.secret,
-    accessTokenExpiresIn: jwtRefreshTokenConstants.expire_in,
-  }),
-);
+export default registerAs('refresh-jwt', () => ({
+  secret: jwtRefreshTokenConstants.secret,
+  expiresIn: jwtRefreshTokenConstants.expire_in,
+}));
