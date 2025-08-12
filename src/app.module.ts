5609f178f7c1ed920b4d0be4e3866efbf727486f
Radek <radoslawlejman@gmail.com>
2025-08-12 23:04:02 +0200

    feat: Added auth module to app module & configure .env in config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './services/prisma.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
