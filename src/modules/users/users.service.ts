b0c4df3e5b665d65b1065db598d6823ab2e2c352
Radek <radoslawlejman@gmail.com>
2025-08-15 17:36:53 +0200

    feat(Users): Added handling for creating users code race

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,14 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, Logger } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/services/prisma.service';
+import { UserLogin } from './interfaces/user.interface';
+import { isUniqueField } from 'src/common/prisma/prisma-errror';
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
   constructor(private readonly prisma: PrismaService) {}
 
   public findByEmail(email: string): Promise<User | null> {
     return this.prisma.user.findUnique({
       where: { email },
     });
+  }
+
+  public async createUser(payload: UserLogin): Promise<User> {
+    try {
+      return await this.prisma.user.create({
+        data: { ...payload },
+      });
+    } catch (error: unknown) {
+      if (isUniqueField(error, ['email'])) {
+        throw new ConflictException('Email already in use');
+      }
+      throw error;
+    }
   }
 }
