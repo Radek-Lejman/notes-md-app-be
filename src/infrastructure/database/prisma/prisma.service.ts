112ba3fd171f0c47b7712ff89119756a149b1361
Radek <radoslawlejman@gmail.com>
2025-10-10 21:45:48 +0200

    chore: remove unnecessary comment

--- src/infrastructure/database/prisma/prisma.service.ts
+++ src/infrastructure/database/prisma/prisma.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-// TODO - upewnij sie ze prisma jest globalnjie dostepna - bo ma w module global
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
